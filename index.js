4dc5b5f3bb992d801b1d59f3851aec3045ddfb27
asiphe-m <42046397+Asi-phe@users.noreply.github.com>
2021-09-14 14:28:07 +0200

    Add files via upload
    final product

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+//running blurring fuction every 30 mili second
+let int = setInterval(blurring, 30)
+let load = 0
+function blurring(){
+    load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    //counting up on the DOM and showing a percentage sign when it's loading
+   loadText.innerText = `${load}%`
+   //fading out the apacity of the text
+   loadText.style.opacity = scale(load, 0,100,1,0)
+   //opacity on the image.
+   bg.style.filter = `blur(${scale(load, 0, 100 , 30,0)}px)`
+}
+//got this function on stack overflow
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
